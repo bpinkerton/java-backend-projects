1ad413e48793bb4a8ca7d69316b92682992923ca
Brandon Pinkerton <brandon.a.pinkerton@gmail.com>
2023-02-13 10:38:10 -0600

    Edit ResourceNotCreatedException.

--- src/main/java/com/revature/exception/ResourceNotCreatedException.java
+++ src/main/java/com/revature/exception/ResourceNotCreatedException.java
@@ -2,7 +2,7 @@ package com.revature.exception;
 
 public class ResourceNotCreatedException extends RuntimeException{
 
-    public ResourceNotCreatedException(String resourceName, Object identifier) {
-        super("Could not create " + resourceName + " with id: " + identifier);
+    public ResourceNotCreatedException(String resourceName) {
+        super("Could not create " + resourceName);
     }
 }
